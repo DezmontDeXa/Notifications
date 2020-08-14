f8cee5de4a649ae2017cae0a7457eabce1490239
DezmontDeXa <flatron2800@gmail.com>
2020-08-14 13:12:47 +0300

    Добавьте файлы проекта.

---
+++ Notifications/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifications
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+
+	public class NotificationsManager
+	{
+		private List<INotificationSender> _senders;
+
+		public NotificationsManager(List<INotificationSender> senders)
+		{
+			_senders = senders;
+		}
+
+		public void SendNotify(string user, string msg, object state = null)
+		{
+			foreach (var sender in _senders)
+			{
+				sender.SendNotify(user, msg, state);
+			}
+		}
+
+		public Task SendNotifyAsync(string user, string msg, object state = null)
+		{
+			return Task.Factory.StartNew(()=> { var u = user; var m = msg; var s = state;  SendNotify(u, m, s); });
+		}
+	}
+
+	public interface INotificationSender
+	{
+		void SendNotify(string user, string msg, object state = null);
+	}
+}
